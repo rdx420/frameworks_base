4990d6d4bca7daad612338c4830bfa61de290b78
SpiritCroc <dev@spiritcroc.de>
2018-11-07 15:02:52 +0100

    Add back more battery bar locations [1/2]
    **SDB- Add option for Below QS Header (For Notch Devices)
    
    Based on orignal battery bar commit by
    cphelps76 <cphelps76@droidconcepts.com>
    
    - Bring to Pie
    - Add bottom of statusbar location
    - Use extra preference for location so we can use our masterswitch
        preference in AE for en-/disabling
    - Leave away rotated navigation bar for now
        depending whether rotated to left or right, battery bar would show
        up on other edge, so would need extra logic
    
    Change-Id: I9b323e025d04a3a16b5557f96f0cd5ad08dce0dc
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,6 +32,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final float mIconAlphaWhenOpaque;
 
     private View mLeftSide, mStatusIcons, mBattery;
+
+    private View mBatteryBars[] = new View[2];
+
     private Animator mCurrentAnimation;
 
     /**
@@ -44,6 +47,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mLeftSide = statusBarView.findViewById(R.id.status_bar_left_side);
         mStatusIcons = statusBarView.findViewById(R.id.statusIcons);
         mBattery = statusBarView.findViewById(R.id.battery);
+        mBatteryBars[0] = statusBarView.findViewById(R.id.battery_bar);
+        mBatteryBars[1] = statusBarView.findViewById(R.id.battery_bar_1);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -86,7 +91,10 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.playTogether(
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
-                    animateTransitionTo(mBattery, newAlphaBC)
+                    animateTransitionTo(mBattery, newAlphaBC),
+		    animateTransitionTo(mBattery, newAlphaBC),
+                    animateTransitionTo(mBatteryBars[0], newAlphaBC),
+                    animateTransitionTo(mBatteryBars[1], newAlphaBC)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -97,6 +105,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
+            mBatteryBars[0].setAlpha(newAlphaBC);
+            mBatteryBars[1].setAlpha(newAlphaBC);
         }
     }
 }
