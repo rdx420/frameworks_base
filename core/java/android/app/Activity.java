45f9eb70484ad0bb97663d9fb6353518ad3211c5
Jon West <electrikjesus@gmail.com>
2020-02-08 15:06:39 -0500

    Add minimize & pip buttons to freeform windows
    Thanks to @farmerbb for assistance and @RadekBledowski for the graphics
    
    Change-Id: I8ddad6eab27f52574ee1c10e8006612fb46a83c5

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -1044,6 +1044,11 @@ public class Activity extends ContextThemeWrapper
             setTaskDescription(mTaskDescription);
         }
 
+        @Override
+        public boolean moveTaskToBack(boolean nonRoot) {
+            return ActivityClient.getInstance().moveActivityTaskToBack(mToken, nonRoot);
+        }
+
     };
 
     private static native String getDlWarning();
@@ -6969,7 +6974,7 @@ public class Activity extends ContextThemeWrapper
      *         back) true is returned, else false.
      */
     public boolean moveTaskToBack(boolean nonRoot) {
-        return ActivityClient.getInstance().moveActivityTaskToBack(mToken, nonRoot);
+        return mWindowControllerCallback.moveTaskToBack(nonRoot);
     }
 
     /**
