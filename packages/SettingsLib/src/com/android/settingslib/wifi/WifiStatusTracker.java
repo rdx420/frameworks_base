1db49be46a6ce9b3142e33851afbe712620ad7ef
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    Wi-Fi timeout feature (1/2)
    SDB - adapt for A12 FLAG_MUTABLE
    
    Issue: calyxos#228
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SettingsLib/src/com/android/settingslib/wifi/WifiStatusTracker.java
+++ packages/SettingsLib/src/com/android/settingslib/wifi/WifiStatusTracker.java
@@ -17,6 +17,7 @@ import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.net.ConnectivityManager;
 import android.net.ConnectivityManager.NetworkCallback;
 import android.net.Network;
@@ -181,6 +182,7 @@ public class WifiStatusTracker {
         mNetworkScoreManager = networkScoreManager;
         mConnectivityManager = connectivityManager;
         mCallback = callback;
+
         if (backgroundHandler == null) {
             HandlerThread handlerThread = new HandlerThread("WifiStatusTrackerHandler");
             handlerThread.start();
@@ -198,6 +200,20 @@ public class WifiStatusTracker {
                         mMainThreadHandler.post(() -> postResults());
                     }
                 };
+
+        mContext.getContentResolver().registerContentObserver(
+                Settings.Global.getUriFor(Settings.Global.WIFI_OFF_TIMEOUT),
+                false,
+                new ContentObserver(mMainThreadHandler) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        super.onChange(selfChange);
+                        WifiTimeoutReceiver.setTimeoutAlarm(context,
+                                Settings.Global.getLong(context.getContentResolver(),
+                                        Settings.Global.WIFI_OFF_TIMEOUT, 0));
+                    }
+                }
+        );
     }
 
     public void setListening(boolean listening) {
@@ -244,6 +260,10 @@ public class WifiStatusTracker {
                 updateRssi(mWifiInfo.getRssi());
                 maybeRequestNetworkScore();
             }
+        } else {
+            WifiTimeoutReceiver.setTimeoutAlarm(mContext,
+                    Settings.Global.getLong(mContext.getContentResolver(),
+                            Settings.Global.WIFI_OFF_TIMEOUT, 0));
         }
         updateStatusLabel();
     }
@@ -255,6 +275,11 @@ public class WifiStatusTracker {
         String action = intent.getAction();
         if (action.equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
             updateWifiState();
+            if (intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN) == WifiManager.WIFI_STATE_ENABLED) {
+                WifiTimeoutReceiver.setTimeoutAlarm(mContext,
+                        Settings.Global.getLong(mContext.getContentResolver(),
+                                Settings.Global.WIFI_OFF_TIMEOUT, 0));
+            }
         }
     }
 
@@ -273,6 +298,10 @@ public class WifiStatusTracker {
             subId = mWifiInfo.getSubscriptionId();
             updateRssi(mWifiInfo.getRssi());
             maybeRequestNetworkScore();
+        } else {
+            WifiTimeoutReceiver.setTimeoutAlarm(mContext,
+                    Settings.Global.getLong(mContext.getContentResolver(),
+                            Settings.Global.WIFI_OFF_TIMEOUT, 0));
         }
     }
 
@@ -386,3 +415,4 @@ public class WifiStatusTracker {
         }
     }
 }
+
