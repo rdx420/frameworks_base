1961cfe92247768b43c53fd0999fd07d12ade48d
spkal01 <kalligeross@gmail.com>
2022-12-31 00:21:17 +0530

    CatalystUtils: Import Settings restart prompt api

--- core/java/com/android/internal/util/catalyst/CatalystUtils.java
+++ core/java/com/android/internal/util/catalyst/CatalystUtils.java
@@ -19,6 +19,8 @@ package com.android.internal.util.catalyst;
 import static android.provider.Settings.Global.ZEN_MODE_OFF;
 import static android.provider.Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS;
 import android.app.ActivityThread;
+import android.app.AlertDialog;
+import android.app.IActivityManager;
 import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -30,6 +32,7 @@ import android.content.Intent;
 import android.content.om.IOverlayManager;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
@@ -45,6 +48,7 @@ import android.os.RemoteException;
 import android.graphics.Color;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.AsyncTask;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.InputDevice;
@@ -70,6 +74,7 @@ import android.widget.Toast;
 import com.android.internal.notification.SystemNotificationChannels;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.R;
+import java.lang.ref.WeakReference;
 
 import java.util.ArrayList;
 
@@ -126,6 +131,51 @@ public class CatalystUtils {
                 enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
         } catch (NameNotFoundException e) {
             return false;
+        }
+    }
+    
+    // Show Restart Prompt
+    public static void showSettingsRestartDialog(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.settings_restart_title)
+                .setMessage(R.string.settings_restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        restartSettings(context);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    public static void restartSettings(Context context) {
+        new restartSettingsTask(context).execute();
+    }
+
+    private static class restartSettingsTask extends AsyncTask<Void, Void, Void> {
+        private Context mContext;
+
+        public restartSettingsTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if ("com.android.settings".equals(app.processName)) {
+                    	ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
     
