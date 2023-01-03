0ba8354c26249f93033b8d7786c22957a015f5b8
Pranav Vashi <neobuddy89@gmail.com>
2023-01-04 03:07:54 +0530

    SystemUI: Update IMS states and update icon when necessary
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -325,8 +325,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     @Override
     void notifyListenersIfNecessary() {
-        mNetworkController.updateImsIcon();
         super.notifyListenersIfNecessary();
+        mNetworkController.updateImsIcon();
     }
 
     void setCarrierNetworkChangeMode(boolean carrierNetworkChangeMode) {
@@ -371,6 +371,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 mImsMmTelManager.registerImsRegistrationCallback(
                         mReceiverHandler::post, mRegistrationCallback);
                 Log.d(mTag, "registerImsRegistrationCallback succeeded");
+                queryImsState();
             } catch (RuntimeException | ImsException e) {
                 if (mRetryCount < MAX_RETRY) {
                     Log.e(mTag, mRetryCount + " registerImsRegistrationCallback failed", e);
@@ -484,6 +485,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         } catch (Exception e) {
             Log.e(mTag, "removeListeners", e);
         }
+        queryImsState();
     }
 
     @Override
--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/NetworkControllerImpl.java
@@ -932,6 +932,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
 
     private void updateMobileControllers() {
         if (!mListening) {
+            updateImsIcon();
             return;
         }
         doUpdateMobileControllers();
@@ -977,6 +978,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
             // Such as on boot, don't need any controllers, because there are no sims,
             // but we still need to update the no sim state.
             updateNoSims();
+            updateImsIcon();
             return;
         }
         synchronized (mLock) {
